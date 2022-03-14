3733f96ed1c4ad5ec45905a3a7e85df53a5bee82
cawooda <cawooda@gmail.com>
2022-03-14 20:27:47 +1100

    Added a Hash function

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const { SHA256 } = require("crypto-js");
+
 class Block {
     constructor(timestamp, lasthash, hash, data) {
         this.timestamp = timestamp;
@@ -12,6 +14,22 @@ class Block {
         Lasthash : ${this.lasthash.substring(0,10)}
         Hash     : ${this.hash.substring(0,10)}
         Data     : ${this.data}`;
+    }
+
+    static genesis() {
+        return new this('Genesis time','--------', 'fir57-h45h',[]);
+    } 
+
+    static mineBlock(lastBlock, data) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        const hash = Block.hash(timestamp,lastHash, data);
+
+        return new this(timestamp, lastHash, hash, data);
+    }
+
+    static hash(timestamp, lastHash, data){
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
     }
 }
 module.exports = Block;
--- dev-test.js
+++ dev-test.js
@@ -1,4 +1,6 @@
+const SHA256 = require('crypto-js/sha256');
+
 const Block = require('./block');
-//const block = new Block();
-const block = new Block('foo', 'bar', 'zoo', 'baz');
-console.log(block.toString());
+
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log(fooBlock.toString());
